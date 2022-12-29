d17a4b83c5b3e96cddd4032e6f75551615923c5f
apollo79 <bummtschack@mail.de>
2022-12-29 14:02:27 +0100

    chore(demo): init gh-pages

--- apps/demo/vite.config.ts
+++ apps/demo/vite.config.ts
@@ -1,6 +1,9 @@
+import { DEV } from "solid-js";
+
 import { defineConfig } from "vite";
 import solid from "vite-plugin-solid";
 
 export default defineConfig({
   plugins: [solid()],
+  base: DEV ? "/" : "/solid-snowfall/",
 });
