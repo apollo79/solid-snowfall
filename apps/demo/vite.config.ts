49f4947e5b95ead1b4e9672baf462b2a573db0b3
apollo79 <bummtschack@mail.de>
2023-01-01 13:35:01 +0100

    fix: layout of color picker (two rows)

--- apps/demo/vite.config.ts
+++ apps/demo/vite.config.ts
@@ -1,9 +1,7 @@
-import { DEV } from "solid-js";
-
 import { defineConfig } from "vite";
 import solid from "vite-plugin-solid";
 
 export default defineConfig({
   plugins: [solid()],
-  base: DEV ? "/" : "/solid-snowfall/",
+  base: "/solid-snowfall/",
 });
